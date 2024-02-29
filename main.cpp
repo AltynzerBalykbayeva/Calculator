781c9600862599d5c9bcd4fc92360e4105515841
AltynzerBalykbayeva <161737544+AltynzerBalykbayeva@users.noreply.github.com>
2024-02-29 21:29:05 +0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// main.cpp
+#include <iostream>
+#include "calculator.h"
+
+int main() {
+    Calculator calculator;
+    // Use the calculator object to perform arithmetic operations
+    return 0;
+}
