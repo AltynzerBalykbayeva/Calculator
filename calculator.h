4f5d118ea2aea01272ecb8ce3de6f450b68028c2
AltynzerBalykbayeva <161737544+AltynzerBalykbayeva@users.noreply.github.com>
2024-02-29 21:29:37 +0600

    Create calculator.h

---
+++ calculator.h
@@ -0,0 +1,11 @@
+// calculator.h
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+    float add(float a, float b);
+    float subtract(float a, float b);
+    float multiply(float a, float b);
+    float divide(float a, float b);
+};
